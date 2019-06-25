3fe533abac2709eaa8402c0a81412083e4eb24d5
Peter J De Sousa <peter.de.sousa@canonical.com>
2019-06-25 18:26:06 +0100

    Add missing paranthesis
    Added missing parenthesis which cause a crash at run-time.

--- reactive/containerd.py
+++ reactive/containerd.py
@@ -273,7 +273,7 @@ def config_changed():
     # See: https://github.com/dshcherb/charm-helpers/blob/eba3742de6a7023f22778ba58fbbb0ac212d2ea6/charmhelpers/core/hookenv.py#L1455
     environment_config = env_proxy_settings()
     if environment_config is not None:
-        config.update(environment_config)
+        config().update(environment_config)
 
 
     config_file = 'config.toml'
