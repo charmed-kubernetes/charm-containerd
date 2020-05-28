12e28741af627680576c506bcb152a307f9abccb
Adam Dyess <adam.dyess@canonical.com>
2020-05-28 12:55:38 -0500

    lp:1879347 patch to apply ca_cert in custom_registries (#39)
    * lp:1879347 patch to apply ca_cert in custom_registries
    
    * lp:1879347 write to appropriate files
    
    * lp:1879347 write tls config if any file has content
    
    * lp:1879347 separate tls config to alternate method
    
    * lp:1879347 fix comments
    
    * lp:1879347 revert check of .idea
    
    * lp:1879347 improve tests
    
    * lp:1879347 improve usability with warning of invalid tls config on custom_registries
    
    * lp:1879347 add insecure_skip_verify to config.toml tls section

--- tests/test_containerd_reactive.py
+++ tests/test_containerd_reactive.py
@@ -1,6 +1,32 @@
+import os
+import json
 from unittest.mock import patch
 from charmhelpers.core import unitdata
 from reactive import containerd
+import tempfile
+
+
+def test_merge_custom_registries():
+    """Verify merges of registries."""
+    with tempfile.TemporaryDirectory() as dir:
+        config = [{
+            "url": "my.registry:port",
+            "username": "user",
+            "password": "pass"
+        }, {
+            "url": "my.other.registry",
+            "ca_file": "aGVsbG8gd29ybGQgY2EtZmlsZQ==",
+            "key_file": "aGVsbG8gd29ybGQga2V5LWZpbGU=",
+        }]
+        ctxs = containerd.merge_custom_registries(dir, json.dumps(config))
+        with open(os.path.join(dir, "my.other.registry.ca")) as f:
+            assert f.read() == "hello world ca-file"
+        with open(os.path.join(dir, "my.other.registry.key")) as f:
+            assert f.read() == "hello world key-file"
+        assert not os.path.exists(os.path.join(dir, "my.other.registry.cert"))
+
+        for ctx in ctxs:
+            assert 'url' in ctx
 
 
 def test_juju_proxy_changed():
