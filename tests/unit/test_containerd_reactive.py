adf1c4a434dbf64035d9c99d4c0b8111db6c8aaf
Cory Johns <johnsca@gmail.com>
2021-06-18 13:35:35 -0400

    Fix unit tests to work with new unitdata support
    The charms.unit_test library was updated to include better support for
    unitdata, removing the need to treat unitdata as a mock.

--- tests/unit/test_containerd_reactive.py
+++ tests/unit/test_containerd_reactive.py
@@ -61,17 +61,15 @@ def test_juju_proxy_changed():
 
     # Test when nothing is cached
     db = unitdata.kv()
-    db.get.return_value = None
     assert containerd._juju_proxy_changed() is True
 
     # Test when cache hasn't changed
-    db.get.return_value = cached
+    db.set('config-cache', cached)
     with patch('reactive.containerd.check_for_juju_https_proxy',
                return_value=cached):
         assert containerd._juju_proxy_changed() is False
 
     # Test when cache has changed
-    db.get.return_value = cached
     with patch('reactive.containerd.check_for_juju_https_proxy',
                return_value=new):
         assert containerd._juju_proxy_changed() is True
