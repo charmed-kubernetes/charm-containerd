71fa897b06ba8a8d1fe8318f494d69e3ef3044ba
Adam Dyess <adam.dyess@canonical.com>
2022-07-20 14:14:51 -0500

    just black (#70)

--- tests/integration/test_containerd_integration.py
+++ tests/integration/test_containerd_integration.py
@@ -29,14 +29,8 @@ async def test_build_and_deploy(ops_test):
     rc, stdout, stderr = await ops_test.run(*shlex.split(cmd))
     assert rc == 0, f"Bundle deploy failed: {(stderr or stdout).strip()}"
 
-    apps = [
-        app
-        for fragment in (bundle, *overlays)
-        for app in yaml.safe_load(fragment.open())["applications"]
-    ]
-    await ops_test.model.wait_for_idle(
-        apps=apps, wait_for_active=True, timeout=60 * 60
-    )
+    apps = [app for fragment in (bundle, *overlays) for app in yaml.safe_load(fragment.open())["applications"]]
+    await ops_test.model.wait_for_idle(apps=apps, wait_for_active=True, timeout=60 * 60)
 
 
 async def test_status_messages(ops_test):
@@ -58,14 +52,19 @@ async def test_upgrade_containerd_action(ops_test):
 @pytest.fixture(scope="module")
 async def juju_config(ops_test):
     """Apply configuration for a test, then revert after the test is completed."""
+
     async def setup(application, **new_config):
-        to_revert[application] = await ops_test.model.applications[application].get_config(), new_config
+        to_revert[application] = (
+            await ops_test.model.applications[application].get_config(),
+            new_config,
+        )
         await ops_test.model.applications[application].set_config(new_config)
         await ops_test.model.wait_for_idle(apps=[application], wait_for_active=True)
+
     to_revert = {}
     yield setup
     for app, (pre_test, settable) in to_revert.items():
-        revert_config = {key: pre_test[key]['value'] for key in settable}
+        revert_config = {key: pre_test[key]["value"] for key in settable}
         await ops_test.model.applications[app].set_config(revert_config)
     await ops_test.model.wait_for_idle(apps=list(to_revert.keys()), wait_for_active=True)
 
@@ -73,7 +72,7 @@ async def juju_config(ops_test):
 @pytest.fixture(scope="module", params=["v1", "v2"])
 async def config_version(request, juju_config):
     """Set the containerd config_version based on a parameter."""
-    await juju_config('containerd', config_version=request.param)
+    await juju_config("containerd", config_version=request.param)
     return request.param
 
 
@@ -104,7 +103,5 @@ async def test_containerd_registry_with_private_registry(config_version, ops_tes
         configs = config["plugins"][plugin]["registry"]["configs"]
         assert len(configs) == 1, "registry config isn't represented in config.toml"
         docker_registry = next(iter(configs))
-        assert configs[docker_registry][
-            "tls"
-        ], "TLS config isn't represented in the config.toml"
+        assert configs[docker_registry]["tls"], "TLS config isn't represented in the config.toml"
         assert docker_registry in registry_unit.workload_status_message
